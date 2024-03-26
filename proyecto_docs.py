e720470b1c21a7f7468e07a03de53a0baa5f1a05
BrunoPM2001 <brunoporras2001@gmail.com>
2024-03-26 15:40:49 -0500

    Conection with mysql ready

--- proyecto_docs.py
+++ proyecto_docs.py
@@ -1,12 +1,19 @@
 # Libs
 import paramiko
+import mysql.connector
 import os
 from dotenv import load_dotenv
 
 # Env vars
 load_dotenv()
+success_count = 0
+error_count = 0
 
+# Download a file using SFTP
 def download_file(remote_path):
+  global success_count
+  global error_count
+
   # Create client
   client = paramiko.SSHClient()
   client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
@@ -23,12 +30,44 @@ def download_file(remote_path):
       os.makedirs(os.getenv('LOC_DIR') + folder)
     sftp.get(remotepath = os.getenv('SFTP_DIR') + remote_path, localpath = os.getenv('LOC_DIR') + remote_path)
 
-    print(f"Downloaded! {remote_path}")
+    success_count += 1
   except Exception as e:
+    error_count += 1
     print(f"Error downloading {remote_path}: {e}")
   finally:
     # Close connection
     sftp.close()
     client.close()
 
-download_file('/files/proyectos/4/4-20170331-211323-1187279183.doc')
+def getList():
+  try:
+    # Connection to mysql
+    conn = mysql.connector.connect(
+      host = os.getenv('MYSQL_HOSTNAME'),
+      user = os.getenv('MYSQL_USERNAME'),
+      password = os.getenv('MYSQL_PASSWORD'),
+      database = os.getenv('MYSQL_DATABASE'),
+      port = os.getenv('MYSQL_PORT')
+    )
+
+    # Query
+    cursor = conn.cursor()
+    cursor.execute(os.getenv('MYSQL_QUERY'))
+    res = cursor.fetchall()
+
+    print("Query executed!")
+    return res
+  except Exception as e:
+    print(f"Error connectiong: {e}")
+  finally:
+    # Close connection
+    conn.close()
+
+# Using both functions
+myList = getList()
+for item in myList:
+  download_file(item[0])
+
+print("Results:")
+print(f"Success: {success_count}")
+print(f"Error: {error_count}")
